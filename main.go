590aebaa23be357daf72ac391d7de7a081b92dd2
Alexander <nyatMeat@gmail.com>
2021-04-03 11:19:54 +0400

    initial

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	blockchain "github.com/nyatmeat/block-test/blockchain"
+)
 
 func main(){
+
+	bc:= blockchain.NewBlockChain()
+	bc.AddBlock("Send 1 block to Ivan")
+	bc.AddBlock("Send 2 block to Ivan")
+
+	for _, block := range bc.blocks{
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
